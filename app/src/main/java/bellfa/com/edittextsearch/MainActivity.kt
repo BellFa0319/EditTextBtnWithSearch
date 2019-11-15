0f81d99d1721709896d3689a5d56c5a031c7467b
pjm3019 <pjm3019@gomcorp.com>
2019-11-15 10:56:38 +0900

    first commit

---
+++ app/src/main/java/bellfa/com/edittextsearch/MainActivity.kt
@@ -0,0 +1,30 @@
+package bellfa.com.edittextsearch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import bellfa.com.edittextsearch.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.buttonSearch.setOnClickListener {
+            binding.edittextSearch.let { binding. textviewResult.setText(binding.edittextSearch.text.toString() + "으로 검색합니다.")}
+        }
+
+        binding.edittextSearch.setOnEditorActionListener(TextView.OnEditorActionListener { v, actionId, event ->
+
+            when(actionId){
+                EditorInfo.IME_ACTION_SEARCH -> binding.buttonSearch.performClick()
+                else -> return@OnEditorActionListener false
+            }
+
+        })
+
+    }
+}
