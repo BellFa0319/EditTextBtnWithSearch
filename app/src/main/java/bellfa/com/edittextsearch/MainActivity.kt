afbc15d61e1fe3e7bc237bc80a64e0e82a2dad80
DESKTOP-44Q7J4B\kk <pjm3019@thegachee.com>
2022-05-27 14:31:08 +0900

    temp5  기능 개발중

--- app/src/main/java/bellfa/com/edittextsearch/MainActivity.kt
+++ app/src/main/java/bellfa/com/edittextsearch/MainActivity.kt
@@ -6,11 +6,11 @@ import android.view.inputmethod.EditorInfo
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import bellfa.com.edittextsearch.databinding.ActivityMainBinding
-///develop 기능 feat 4 기능 완료 4-1 핫픽스
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        //temp5 기능 개발중
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.buttonSearch.setOnClickListener {
